4d47fcf86287f0864f8653b205b3b4639b38608e
Stephany Bolivar <g.stephanybolivar@gmail.com>
2020-12-01 20:35:12 -0500

    Set up orm queries

---
+++ config/orm.js
@@ -0,0 +1,74 @@
+const connection = require("../config/connection");
+// Helper function loops through and creates an array of question marks and turns it into a string
+function printQuestionMarks(num) {
+  let arr = [];
+
+  for (let i = 0; i < num; i++) {
+    arr.push("?");
+  }
+  return arr.toString();
+}
+// Helper function to convert object key/value pairs to SQL syntax
+function objToSql(ob) {
+  let arr = [];
+
+  for (var key in ob) {
+    const value = ob[key];
+
+    if (Object.hasOwnProperty.call(ob, key)) {
+      if (typeof value === "string" && value.indexOf(" ") >= 0) {
+        value = "'" + value + "'";
+      }
+      arr.push(key + "=" + value);
+    }
+  }
+  return arr.toString();
+}
+
+const orm = {
+  all: (tableInput, cb) => {
+    const queryString = "SELECT * FROM " + tableInput + ";";
+    connection.query(queryString, function (err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  },
+  create: (table, cols, vals, cb) => {
+    const queryString = "INSERT INTO " + table;
+
+    queryString += " (";
+    queryString += cols.toString();
+    queryString += ") ";
+    queryString += "VALUES (";
+    queryString += printQuestionMarks(vals.length);
+    queryString += ") ";
+
+    console.log(queryString);
+    connection.query(queryString, vals, function (err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  },
+  update: (table, objColVals, condition, cb) => {
+    const queryString = "UPDATE " + table;
+
+    queryString += " SET ";
+    queryString += objToSql(objColVals);
+    queryString += " WHERE ";
+    queryString += condition;
+
+    console.log(queryString);
+    connection.query(queryString, function (err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  },
+};
+
+module.exports = orm;
