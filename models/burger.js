2343472fb6b6ca59a695dd189f6f79e624e43e80
Stephany Bolivar <g.stephanybolivar@gmail.com>
2020-12-01 20:49:10 -0500

    Created functions that will interact with the burgers database

---
+++ models/burger.js
@@ -0,0 +1,22 @@
+// Import ORM to create functions that will interact with burgers_db
+const orm = require("../config/orm");
+
+const burger = {
+    all: function(cb){
+        orm.all("burgers", function(res) {
+            cb(res);
+        });
+    },
+    create: function(cols, vals, cb){
+        orm.create("burgers", cols, vals,function(res){
+            cb(res);
+        });
+    },
+    update: function(objColVals, condition, cb){
+        orm.update("burgers", objColVals, condition, function(res) {
+            cb(res);
+        });
+    }
+};
+// Export database functions for the burger controller
+module.exports = burger;
