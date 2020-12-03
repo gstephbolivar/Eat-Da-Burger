06a24f60f7d3ce81923474c0c32c4089d5a89ab6
Stephany Bolivar <g.stephanybolivar@gmail.com>
2020-12-03 18:39:22 -0500

    Allow for public folders to be static

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ app.use(express.static("public"));
 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
+app.use(express.static("public"));
 
 const exphbs = require("express-handlebars");
 
